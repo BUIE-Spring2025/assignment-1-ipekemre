28acd64ad30211c7bf332a9a024e7cd45775d9db
ipekemre <ipekemre26@gmail.com>
2025-03-02 18:29:32 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -5,3 +5,25 @@ def int_to_roman(num):
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
+    roman_symbols = [
+        (1000, 'M'),
+        (900, 'CM'),
+        (500, 'D'),
+        (400, 'CD'),
+        (100, 'C'),
+        (90, 'XC'),
+        (50, 'L'),
+        (40, 'XL'),
+        (10, 'X'),
+        (9, 'IX'),
+        (5, 'V'),
+        (4, 'IV'),
+        (1, 'I')
+    ]
+    result = ''
+    for val, symbol in roman_symbols:
+        while num >= val:
+            result += symbol
+            num -= val
+    
+    return result
